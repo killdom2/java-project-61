af5417b077fde52090a06d5d1d192bdeac2698ca
killdom2 <kldm-2@mail.ru>
2024-08-31 15:57:59 +0400

    The third step of the project is completed

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,10 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Wellcome to the Brain Games!");
+
+        System.out.print("Wellcome to the Brain Games!\nMay I have your name? ");
+
+        Cli.greeting();
+
     }
 }
