b8598fdddf5e64beb927257292e0428f79677c96
killdom2 <kldm-2@mail.ru>
2024-08-31 22:09:42 +0400

    Checkstyle & CodeClimate connected

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,12 +1,12 @@
 package hexlet.code;
+
 import java.util.Scanner;
 
 public class Cli {
-	static void greeting() {
-		Scanner scan = new Scanner(System.in);
-		var userName = scan.next();
-		scan.close();
-		System.out.println("Hello, " + userName + "!");
-		return;
-	}
+    static void greeting() {
+        Scanner scan = new Scanner(System.in);
+        var userName = scan.next();
+        scan.close();
+        System.out.println("Hello, " + userName + "!");
+    }
 }
