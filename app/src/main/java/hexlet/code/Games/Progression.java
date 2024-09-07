7282affc71e30f825d7a061da884cb99a0db56cf
killdom2 <kldm-2@mail.ru>
2024-09-08 01:44:28 +0400

    Try fix 'codeclimate' issues

--- app/src/main/java/hexlet/code/Games/Progression.java
+++ app/src/main/java/hexlet/code/Games/Progression.java
@@ -3,61 +3,60 @@ import hexlet.code.Engine;
 public class Progression {
     public static void progression() {
         System.out.println("What number is missing in the progression?");
-        var stepOfProgression = 0;
-        var firstNumber = 0;
-        var lengthOfProgression = 0;
+        int step;
+        int firstNumber;
+        int length;
         var maxLength = 10;
         var difficulty = 30;
         var i = 0;
         var gameNumber = 0;
-        var hiddenNumber = 0;
+        var hiddenNum = 0;
         while (gameNumber < 3) {
             do {
                 firstNumber = Engine.randomNumber(difficulty);
-                stepOfProgression = Engine.randomNumber(maxLength);
-                lengthOfProgression = Engine.randomNumber(maxLength);
-                if (correctLengthAndStep(lengthOfProgression, stepOfProgression)) {
+                step = Engine.randomNumber(maxLength);
+                length = Engine.randomNumber(maxLength);
+                if (correctLengthAndStep(length, step)) {
                     i = 1;
-                } else if (lessThenFive(lengthOfProgression, stepOfProgression)) {
-                    lengthOfProgression = 10;
+                } else if (recommendTen(length, step)) {
+                    length = 10;
                     i = 1;
                 } else {
                     i = 0;
                 }
             } while (i == 0);
-            hiddenNumber = Engine.randomNumber(lengthOfProgression);
-            var progression = new int[lengthOfProgression];
+
+            hiddenNum = Engine.randomNumber(length);
             System.out.print("Question: ");
-            progression = makeArray(lengthOfProgression, firstNumber, stepOfProgression);
-            printNumbers(progression, lengthOfProgression, hiddenNumber);
+
+            hiddenNum = printProgression(length, hiddenNum, firstNumber, step);
+
             System.out.print("\nYour answer: ");
             var userAnswer = Engine.userInput();
-            gameNumber = Engine.correctOrNot(userAnswer, Integer.toString(progression[hiddenNumber]), gameNumber);
+            gameNumber = Engine.correctOrNot(userAnswer, Integer.toString(hiddenNum), gameNumber);
         }
     }
-    static boolean correctLengthAndStep(int lengthOfProgression, int stepOfProgression) {
-        return lengthOfProgression >= 5 && lengthOfProgression <= 10 && stepOfProgression > 1;
+
+    private static boolean correctLengthAndStep(int length, int step) {
+        return length >= 5 && length <= 10 && step > 1;
     }
-    static boolean lessThenFive(int lengthOfProgression, int stepOfProgression) {
-        return lengthOfProgression < 5 && stepOfProgression > 1;
+
+    private static boolean recommendTen(int length, int step) {
+        return length < 5 && step > 1;
     }
-    static void printNumbers(int[] progression, int lengthOfProgression, int hiddenNumber) {
-        for (int i = 0; i < lengthOfProgression; i++) {
-            if (i == hiddenNumber) {
+
+    private static int printProgression(int length, int hiddenNum, int firstNumber, int step) {
+        var num = 0;
+        for (int i = 0; i < length; i++) {
+            if (i == hiddenNum) {
                 System.out.print(".. ");
+                num = firstNumber;
             } else {
-                System.out.print(progression[i] + " ");
+                System.out.print(firstNumber + " ");
             }
+            firstNumber += step;
         }
-    }
-    static int[] makeArray(int lengthOfProgression, int firstNumber, int stepOfProgression) {
-        var count = 0;
-        var progression = new int[lengthOfProgression];
-        while (count < lengthOfProgression) {
-            progression[count] = firstNumber;
-            firstNumber += stepOfProgression;
-            count++;
-        }
-        return progression;
+        return num;
     }
 }
+
